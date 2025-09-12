ac06d47cd207ca40d1d3776b574b1363c1bfd414
mark <markoc1120@gmail.com>
2025-09-12 17:22:08 +0200

    feat: added handlerChirpsGet

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
 	chirps, err := cfg.db.GetChirps(r.Context())
@@ -20,4 +25,29 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 		})
 	}
 	respondWithJSON(w, http.StatusOK, payload)
+}
+
+func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	query := r.PathValue("chirpID")
+	id, err := uuid.Parse(query)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Invalid chirp ID in the url", err)
+		return
+	}
+	chirp, err := cfg.db.GetChirp(r.Context(), id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			respondWithError(w, http.StatusNotFound, "chirp not found", nil)
+			return
+		}
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve the single chirp instance from db", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, Chirp{
+		ID:        chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body:      chirp.Body,
+		UserID:    chirp.UserID,
+	})
 }
