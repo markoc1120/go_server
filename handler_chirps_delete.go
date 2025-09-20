7636c0c89179650eb6c68be0b5de61f2a8d71f83
mark <markoc1120@gmail.com>
2025-09-20 08:15:39 +0200

    feat: added /api/chirps/{chirpID} [DELETE]

---
+++ handler_chirps_delete.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/markoc1120/go_server/internal/auth"
+)
+
+func (cfg *apiConfig) handlerChirpsDelete(w http.ResponseWriter, r *http.Request) {
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find token", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.secret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate token", err)
+		return
+	}
+
+	query := r.PathValue("chirpID")
+	chirpID, err := uuid.Parse(query)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Invalid chirp ID in the url", err)
+		return
+	}
+
+	chirp, err := cfg.db.GetChirp(r.Context(), chirpID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			respondWithError(w, http.StatusNotFound, "chirp not found", nil)
+			return
+		}
+		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve the single chirp instance from db", err)
+		return
+	}
+
+	if chirp.UserID != userID {
+		respondWithError(w, http.StatusForbidden, "You can't do this", err)
+		return
+	}
+
+	err = cfg.db.DeleteChirp(r.Context(), chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't delete the chirp instance from db", err)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
