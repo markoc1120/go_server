e76cc06662b5c781c761097dafb40ed01fb3615c
mark <markoc1120@gmail.com>
2025-09-22 17:05:57 +0200

    feat: added author_id query parameter handling in /api/chirps [GET]

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"net/http"
 
 	"github.com/google/uuid"
+	"github.com/markoc1120/go_server/internal/database"
 	"github.com/markoc1120/go_server/internal/models"
 	"github.com/markoc1120/go_server/internal/response"
 )
 
+func getChirpInstances(ctx context.Context, userID *uuid.UUID, db *database.Queries) ([]database.Chirp, error) {
+	if userID != nil {
+		return db.GetChirpsByUserID(ctx, *userID)
+	}
+	return db.GetChirps(ctx)
+}
+
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
-	chirps, err := cfg.db.GetChirps(r.Context())
+	ctx := r.Context()
+
+	var userID *uuid.UUID
+	if authorID := r.URL.Query().Get("author_id"); authorID != "" {
+		id, err := uuid.Parse(authorID)
+		if err != nil {
+			response.WithError(w, http.StatusBadRequest, "Invalid author_id query parameter", err)
+			return
+		}
+		userID = &id
+	}
+
+	chirps, err := getChirpInstances(ctx, userID, cfg.db)
 	if err != nil {
 		response.WithError(w, http.StatusInternalServerError, "Couldn't retrieve all instances from db", err)
 		return
@@ -33,7 +54,7 @@ func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
 	query := r.PathValue("chirpID")
 	id, err := uuid.Parse(query)
 	if err != nil {
-		response.WithError(w, http.StatusInternalServerError, "Invalid chirp ID in the url", err)
+		response.WithError(w, http.StatusBadRequest, "Invalid chirpID in the url", err)
 		return
 	}
 	chirp, err := cfg.db.GetChirp(r.Context(), id)
